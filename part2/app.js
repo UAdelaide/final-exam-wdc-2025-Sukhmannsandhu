242a1bcfbfeb05356d50b1d8324f46d7bd223dca
Sukhmannsandhu <a1913187@adelaide.edu.au>
2025-06-20 13:08:52 +0000

    [automatic commit] 2025-06-20 13:08:52 UTC 36.255.115.0

--- part2/app.js
+++ part2/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const pool = require('./models/db');
+const userRoutes = require('./routes/userRoutes');
+const walkRoutes = require('./routes/walkRequests');
+const dogRoutes = require('./routes/dogRoutes');
+
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 
+// Root route (test server)
 app.get('/', (req, res) => {
   res.json({ message: 'Server is working with MySQL2!' });
 });
 
-// Route to test all users
+// Route to get all users
 app.get('/users', async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM Users');
+    console.log('📦 Users:', rows);
     res.json(rows);
   } catch (err) {
     console.error('DB error:', err);
@@ -20,10 +26,11 @@ app.get('/users', async (req, res) => {
   }
 });
 
-// 🔗 Import the walkrequests route
-const walkRequestRoutes = require('./routes/walkrequests');
-app.use('/api/walkrequests', walkRequestRoutes);
+// Custom API routes
+app.use('/api/dogs', dogRoutes);
+app.use('/api/walkrequests', walkRoutes);
+// app.use('/api/walkers', walkerRoutes); ← You'll add this for Q8
 
 app.listen(port, () => {
   console.log(`✅ Server is running on port ${port}`);
-});
+})
