a6406b157eae77dad76606e9c4fc8f5f27ba50d0
Sukhmannsandhu <a1913187@adelaide.edu.au>
2025-06-20 13:07:32 +0000

    [automatic commit] 2025-06-20 13:07:32 UTC 36.255.115.0

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../models/db');
+const db = require('../models/db');
 
 // GET /api/walkrequests/open
 router.get('/open', async (req, res) => {
   try {
-    const [rows] = await pool.query(`
+    const [rows] = await db.query(`
       SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
