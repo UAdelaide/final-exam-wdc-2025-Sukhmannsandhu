e3316f0b7a1babcf4975dc391a7f33e1817036d6
Sukhmannsandhu <a1913187@adelaide.edu.au>
2025-06-20 20:01:49 +0000

    db.js

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',          // or your MySQL root password
+  password: '' // or your MySQL root password
   database: 'DogWalkService'
 });
 
