2a6944d5af4c4f3263be6c6b7ec55587a196def8
Sukhmannsandhu <a1913187@adelaide.edu.au>
2025-06-20 13:14:55 +0000

    app.js

--- part2/app.js
+++ part2/app.js
@@ -1,14 +1,27 @@
+const express = require('express');
+const pool = require('./models/db');
+const userRoutes = require('./routes/userRoutes');
+const walkRoutes = require('./routes/walkRoutes');
+const dogRoutes = require('./routes/dogRoutes');
+const walkerRoutes = require('./routes/walkerRoutes');
 
-    console.error('DB error:', err);
-    res.status(500).json({ error: 'Database error' });
-  }
+const app = express();
+const port = 8080;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Root test route
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is working with MySQL2!' });
 });
 
-// Custom API routes
+// Base routes
+app.use('/users', userRoutes);
 app.use('/api/dogs', dogRoutes);
 app.use('/api/walkrequests', walkRoutes);
-// app.use('/api/walkers', walkerRoutes); ← You'll add this for Q8
+app.use('/api/walkers', walkerRoutes);
 
 app.listen(port, () => {
   console.log(`✅ Server is running on port ${port}`);
-})
+});
