1e460ef4d0851e9fa287d52517a60b441a152413
Sukhmannsandhu <a1913187@adelaide.edu.au>
2025-06-20 12:43:52 +0000

    [automatic commit] 2025-06-20 12:43:52 UTC 36.255.115.0

--- part2/app.js
+++ part2/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const pool = require('./models/db');
+const userRoutes = require('./routes/userRoutes');
+const walkRoutes = require('./routes/walkRoutes');
+const dogRoutes = require('./routes/dogRoutes');
+
 const app = express();
 const port = 8080;
+
+app.use(express.json());
 
 // Root test route
 app.get('/', (req, res) => {
   res.json({ message: 'Server is working with MySQL2!' });
 });
 
-// Route to get all users
+// Users test route
 app.get('/users', async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM Users');
@@ -19,6 +25,11 @@ app.get('/users', async (req, res) => {
     res.status(500).json({ error: 'Database error' });
   }
 });
+
+// ✅ Connect API routes
+app.use('/api', userRoutes);
+app.use('/api', walkRoutes);
+app.use('/api', dogRoutes);
 
 app.listen(port, () => {
   console.log(`✅ Server is running on port ${port}`);
